8af852fd89e906cc4bb4d82101c53b8803154ac9
qianhuiya <2867463524@qq.com>
2020-11-30 17:04:05 +0800

    PicQh

---
+++ src/components/Header.vue
@@ -0,0 +1,178 @@
+<template>
+  <header class="header">
+    <div class="brand" @click="$router.push('/')">
+      <div class="logo">
+        <img :src="logoImage" alt="PicX"/>
+      </div>
+      <span class="name">
+        PicQh
+      </span>
+    </div>
+
+    <div class="user-info" @click="onUserInfoClick">
+
+      <div class="username">
+        {{ userConfigInfo.owner ? userConfigInfo.owner : defaultUsername }}
+      </div>
+
+      <el-dropdown trigger="click"
+                   @command="handleCommand"
+      >
+        <span class="el-dropdown-link">
+          <span class="avatar">
+            <i class="el-icon-user-solid" v-if="!userConfigInfo.avatarUrl"></i>
+            <img :src="userConfigInfo.avatarUrl"
+                 :alt="userConfigInfo.owner"
+                 v-if="userConfigInfo.avatarUrl"
+            />
+          </span>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item
+            v-if="userConfigInfo.loggingStatus"
+            command="logout"
+          >退出登录
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </header>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "Header",
+
+  data() {
+    return {
+      logoImage: "https://cdn.jsdelivr.net/gh/qianhuiya/picgo@main/avaer/2.4riw20pxxqi0.jpg",
+      defaultUsername: '未登录',
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      userConfigInfo: 'getUserConfigInfo'
+    }),
+  },
+
+  mounted() {
+  },
+
+  methods: {
+    onUserInfoClick() {
+      if (!this.userConfigInfo.loggingStatus && this.$router.currentRoute.path !== '/config') {
+        this.$router.push('/config')
+      }
+    },
+
+    handleCommand(command) {
+      switch (command) {
+        case 'upload':
+          this.$router.push('/')
+          break;
+
+        case 'config':
+          this.$router.push('config')
+          break;
+
+        case 'management':
+          this.$router.push('management')
+          break;
+
+        case 'logout':
+          this.logout()
+          break;
+      }
+    },
+
+    logout() {
+      this.$router.push('config')
+      this.$store.dispatch('LOGOUT')
+    },
+
+    goGitHubRepo() {
+      window.open('https://github.com/XPoet/picx');
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+@import "src/style";
+
+.header {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: $headerFontColor;
+
+  .brand {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    .logo {
+      width: 48px;
+      height: 48px;
+      margin-right: 8px;
+
+      img {
+        width: 100%;
+      }
+    }
+
+    .name {
+      font-size: 30px;
+      margin-right: 10px;
+    }
+
+    .github-stars {
+      margin-left: 18px;
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  .user-info {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    .username {
+      font-size: 16px;
+    }
+
+    .avatar {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 38px;
+      height: 38px;
+      color: $headerFontColor;
+      border-radius: 50%;
+      border: 1px solid $headerFontColor;
+      margin-left: 10px;
+      padding: 1px;
+      box-sizing: border-box;
+
+      i {
+        font-size: 28px;
+      }
+
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
+    }
+
+  }
+
+}
+</style>
---
+++ src/views/Help.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="page-container feedback-page-container">
+
+    <div class="help-info-item description">
+      PicQh，基于 GitHub API 搭建的图床神器，图片外链自动使用 jsDelivr 进行 CDN 加速，免费、稳定、高效。
+      <br>
+      只需一个 GitHub 账号，免下载安装，打开网站即可使用，轻松解决图床难题。
+      <br>
+      建议将此网站添加至收藏夹，方便下次使用 😊
+    </div>
+
+    <div class="help-info-item">
+      作者：
+      <el-link type="primary" href="https://qianhuiya.gitee.io/" target="_blank">@qianhuiya</el-link>
+    </div>
+
+    <div class="help-info-item">
+      仓库：
+      <el-link type="primary" href="https://qianhuiya.gitee.io/" target="_blank">
+        https://qianhuiya.gitee.io/
+      </el-link>
+    </div>
+
+    <div class="help-info-item">
+      声明：请勿通过此网站上传违反你当地法律的图片，一切后果与此网站无关。
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Help",
+}
+</script>
+
+<style scoped lang="scss">
+
+.feedback-page-container {
+  .help-info-item {
+    font-size: 15.8px;
+    padding: 6px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 10px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+
+
+  }
+
+  .description {
+    font-weight: bold;
+    line-height: 28px;
+  }
+
+  .red-text {
+    color: #de1a1a;
+  }
+}
+
+</style>
